3d17a08803cc23dda1a67377ed37487758902e75
Diego Angulo Ramirez <diegoangulo5@gmail.com>
2023-05-20 22:59:10 +0300

    Added encryption for messages

--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,4 @@
+from cryptography.fernet import Fernet
 from django.shortcuts import render, redirect
 
 from .models import Messages
@@ -29,6 +30,30 @@ def chat(request, username):
         return render(request, "chats/chats.html", context)
 
 
+def encrypt(message, key):
+    f = Fernet(key)
+    encrypted_message = f.encrypt(message.encode('utf-8'))
+    return encrypted_message.decode('utf-8')
+
+
+def decrypt(message, key):
+    f = Fernet(key)
+    decrypted_message = f.decrypt(message.encode('utf-8'))
+    return decrypted_message.decode('utf-8')
+
+
+def check_key(user1, user2):
+    if Friends.objects.get(user=user1, friend=user2).encryption_key == "":
+        friends = Friends.objects.get(user=user1, friend=user2)
+        friends.encryption_key = Fernet.generate_key()
+        friends.encryption_key = friends.encryption_key.decode('utf-8')
+        friends.save()
+        friends1 = Friends.objects.get(user=user2, friend=user1)
+        friends1.encryption_key = friends.encryption_key
+        friends1.save()
+    return Friends.objects.get(user=user1, friend=user2).encryption_key
+
+
 @csrf_exempt
 def message_list(request, sender=None, receiver=None):
     if not request.user.is_authenticated:
@@ -44,12 +69,24 @@ def message_list(request, sender=None, receiver=None):
         )
         for message in messages:
             message.seen = True
+            print(sender, receiver)
+            print(message.sender_name, message.receiver_name)
+            sender = User.objects.get(id=sender)
+            receiver = User.objects.get(id=receiver)
+            key = check_key(sender, receiver).encode('utf-8')
+            message.message = decrypt(message.message, key)
             message.save()
         return JsonResponse(serializer.data, safe=False)
 
     elif request.method == "POST":
         # breakpoint()
         data = JSONParser().parse(request)
+
+        sender = User.objects.get(username=data["sender_name"])
+        receiver = User.objects.get(username=data["receiver_name"])
+        key = check_key(sender, receiver).encode('utf-8')
+        data["message"] = encrypt(data["message"], key)
+        print("DATA: ", data)
         serializer = MessageSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
